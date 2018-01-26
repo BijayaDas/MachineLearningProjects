07f95c4f685e52d887ff0231a35d52273cfdf48f
= <bkdas@teamdrg.com>
2018-01-26 10:41:37 +0530

    practice yesterday

---
+++ hello-world.r
@@ -0,0 +1,17 @@
+g2 <<- c (2,1,TRUE, 2+3i)
+
+g1 <- c (2, 1, TRUE, 2 + 3i)
+
+gk  <-  switch (
+  2,
+  "First",
+  "Second",
+  "Third",
+  "Fourth"
+)
+print (gk)
+
+hypotenuse <- function (x, y)
+{
+  sqrt (x ^ 2 + y ^ 2)
+}
